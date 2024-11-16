fd9717cc597a6ff2e12ee072b827c1ae7b237e94
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2024-11-16 14:48:36 +0900

    removed tx

---
+++ sql/index_ddl/indexes.sql
@@ -0,0 +1,5 @@
+CREATE INDEX reactions_livestream_id_idx ON reactions(`livestream_id`);
+CREATE INDEX reservation_slots_between_start_end ON reservation_slots(`start_at`, `end_at`);
+CREATE INDEX livecomment_reports_livestream_id_idx ON livecomment_reports(`livestream_id`);
+CREATE INDEX livestream_tags_livestream_id_idx ON livestream_tags(`livestream_id`);
+CREATE INDEX ng_words_user_id_and_livestream_id_idx ON ng_words(`user_id`, `livestream_id`);
